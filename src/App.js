effd47298c44e8c72af814557063de988894a20c
smshen <thanatos1710@gmail.com>
2017-08-25 16:31:31 +0800

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import './App.css'
+import SignUpForm from './form/SignUpForm.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className='App'>
+          <Link to='/#1'>Multilevel Form</Link>
+          <Route path='/' component={SignUpForm}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/form/SignUpForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SignUpForm extends Component {
+
+  
+  render() {
+    console.log(this)
+    return (
+      <form>
+        <input></input>
+      </form>
+    );
+  }
+}
+
+export default SignUpForm;
